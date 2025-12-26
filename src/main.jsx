ceaa21c055d266853bfb3eebad33e1ac07135d3c
yadam suresh <ysuresh@yadams-Laptop.local>
2025-12-26 10:40:36 +0530

    added react-bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+//we are adding it above the index.css file because we can override the bootstrap styles with our application styles...
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);

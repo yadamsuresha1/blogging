4be37d88e0ccc4d25daed35410f2b8f75b2236e7
yadam suresh <ysuresh@yadams-Laptop.local>
2025-12-31 05:57:18 +0530

    backend logic and generic components

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,25 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import ExplorePage from "./pages/Explorepage";
 import LoginPage from "./pages/LoginPage";
 import UserPage from "./pages/UserPage";
+import ApiProvider from "./contexts/ApiProvider";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<FeedPage />} />
-        <Route path="/explore" element={<ExplorePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/user/:username" element={<UserPage />} />
-        <Route path="/*" element={<Navigate to={"/"} />} />
-      </Routes>
-      {/* <Container fluid className="App">
+      <ApiProvider>
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<FeedPage />} />
+          <Route path="/explore" element={<ExplorePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/user/:username" element={<UserPage />} />
+          <Route path="/*" element={<Navigate to={"/"} />} />
+        </Routes>
+        {/* <Container fluid className="App">
         <FeedPage />
       </Container> */}
+      </ApiProvider>
     </BrowserRouter>
   );
 };
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,23 +1,54 @@
 import { useEffect, useState } from "react";
 import { Spinner } from "react-bootstrap";
 import Post from "./Post";
+import { useApi } from "../hooks/useApi";
+import More from "./More";
 
 // const BASE_API_URL = process.env.REACT_APP_BASE_API_URL;
 const BASE_API_URL = import.meta.env.VITE_BASE_API_URL;
-export default function Posts() {
+export default function Posts({ content }) {
   const [posts, setPosts] = useState();
+  const [pagination, setPagination] = useState();
+  const api = useApi();
+  let url;
+  switch (content) {
+    case "feed":
+    case undefined:
+      url = "/feed";
+      break;
+    case "explore":
+      url = "/posts";
+      break;
+    default:
+      url = `/users/${content}/posts`;
+      break;
+  }
 
   useEffect(() => {
     (async () => {
-      const response = await fetch(BASE_API_URL + "/api/feed");
+      //   const response = await fetch(BASE_API_URL + "/api/feed");
+      const response = await api.get(url);
+      console.log("responsetop", response);
       if (response.ok) {
-        const results = await response.json();
-        setPosts(results.data);
+        console.log("response", response);
+        // const results = await response.body.data;
+        setPosts(response.body.data);
+        setPagination(response.body.pagination);
       } else {
         setPosts(null);
       }
     })();
-  }, []);
+  }, [api, url]);
+
+  const loadNextPage = async () => {
+    const response = await api.get(url, {
+      after: posts[posts.length - 1].timestamp,
+    });
+    if (response.ok) {
+      setPosts([...posts, ...response.body.data]);
+      setPagination(response.body.pagination);
+    }
+  };
   return (
     <>
       {posts === undefined ? (
@@ -33,6 +64,7 @@ export default function Posts() {
               ) : (
                 posts.map((post) => <Post key={post.id} post={post} />)
               )}
+              <More pagination={pagination} loadNextPage={loadNextPage} />
             </>
           )}
         </>
